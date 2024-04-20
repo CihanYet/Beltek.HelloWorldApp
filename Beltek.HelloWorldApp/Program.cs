304fc064ddb2df399c64fe741a3f16c0a50ef4fc
GEF-LAB1 <GEF-LAB1@DESKTOP-1V3DLAU>
2024-04-20 12:55:08 +0300

    Değişken örneği

--- Beltek.HelloWorldApp/Program.cs
+++ Beltek.HelloWorldApp/Program.cs
@@ -1,12 +1,25 @@
-﻿namespace Beltek.HelloWorldApp
+﻿using System;
+namespace Beltek.HelloWorldApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Adınızı giriniz:");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Hoşgeldin," + isim);
+
+            Console.ReadKey();
         }
     }
 }
 
 //Solution: İçerisinde birden fazla proje bulundurabilen dosyalardır. Solution dosyaları, bu projelerin disk üzerinde kayıtlı oldugu yer bilgisini tutarlar(Path).
+//namespace: İçinde classları bulunduran yapılardır.
+//{} Scope(Blok) Yapıları:Bu yapılar içerisine birden fazla satır kod yazılabilir.
+//class: İçinde metodları bulunduran yapıladır(Şimdilik bu tanım)
+//Metod: İş yapan kod bloklarıdır. Main metodu, console uygulamalarının giriş noktasıdır.
+//Syntax: Kod yazım kuralları
+
+//C#->Derleme(Compile)->Makina Dili(0,1)
+//C#->Derleme->MS-IL->Runtime->Derleme->0,1
