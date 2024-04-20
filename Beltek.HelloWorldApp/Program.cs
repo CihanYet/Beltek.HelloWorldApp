dead23db6c2f93feb25c8c8cdef43cfaf97910a9
GEF-LAB1 <GEF-LAB1@DESKTOP-1V3DLAU>
2024-04-20 10:46:39 +0300

    Add project files.

---
+++ Beltek.HelloWorldApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Beltek.HelloWorldApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+//Solution: İçerisinde birden fazla proje bulundurabilen dosyalardır. Solution dosyaları, bu projelerin disk üzerinde kayıtlı oldugu yer bilgisini tutarlar(Path).
