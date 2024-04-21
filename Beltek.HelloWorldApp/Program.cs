2ec67f6586e34779bbfdbc628e06867457f7fb48
GEF-LAB1 <GEF-LAB1@DESKTOP-1V3DLAU>
2024-04-21 12:55:48 +0300

    Tür Dönüşümleri

--- Beltek.HelloWorldApp/Program.cs
+++ Beltek.HelloWorldApp/Program.cs
@@ -3,16 +3,75 @@ namespace Beltek.HelloWorldApp
 {
     internal class Program
     {
+        static int number = 10;
         static void Main(string[] args)
         {
-            Console.WriteLine("Adınızı giriniz:");
+            #region Değişkenler
+            //Console.WriteLine("Adınızı giriniz:");
+            //string isim = Console.ReadLine();
+            //Console.WriteLine("Hoşgeldin," + isim);
+
+            //byte yas;
+            //Console.WriteLine(yas);
+
+            //Int16 sayi = 5;
+            //Int32 _sayi = 5;
+            //Int64 __sayi = 5;
+
+            //byte sayi = 50;//Explicit
+            //var _sayi = 50;//Implicit
+
+            //string isim = "Ahmet";
+            //string soyad = "Mehmet";
+
+            //Console.Write(isim);
+            //Console.WriteLine(soyad);
+
+            //Console.WriteLine("Hoşgeldin, " + isim + " " + soyad);
+            //Console.WriteLine("Hoşgeldin, {0} {1}", isim, soyad);
+            //Console.WriteLine($"Hoşgeldin, {isim} {soyad}"); 
+            #endregion
+
+            Console.WriteLine("Adınızı Giriniz:");
             string isim = Console.ReadLine();
-            Console.WriteLine("Hoşgeldin," + isim);
+            Console.WriteLine("Soyadını Giriniz:");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("Yaşınızı giriniz:");
+            byte yas = byte.Parse(Console.ReadLine());
+            Console.WriteLine($"Adınız:{isim}\nSoyadınız:{soyad}\nYaşınız:{yas}");
+
+
+
+            //****Tür Dönüşümleri(Type Casting)****
+            //Type-Safety
+
+            //byte sayi = 5;
+            //int number = sayi;//Implicit Type Casting
+
+            //checked
+            //{
+            //    int sayi = 257;
+            //    byte number = (byte)sayi;//Explicit Type Casting
+            //    Console.WriteLine(number); 
+            //}
+
+            //string sayi = "25";
+            //byte number = Convert.ToByte(sayi);
+
+            //byte _number = byte.Parse(sayi);
+
+            //int sayi = 50;
+            //object number = sayi;//Boxing
+            //int sayi2 = 10;
+            //int sonuc = (int)number + sayi2;//Unboxing
+
+
 
             Console.ReadKey();
         }
     }
 }
+
 
 //Solution: İçerisinde birden fazla proje bulundurabilen dosyalardır. Solution dosyaları, bu projelerin disk üzerinde kayıtlı oldugu yer bilgisini tutarlar(Path).
 //namespace: İçinde classları bulunduran yapılardır.
@@ -23,3 +82,6 @@ namespace Beltek.HelloWorldApp
 
 //C#->Derleme(Compile)->Makina Dili(0,1)
 //C#->Derleme->MS-IL->Runtime->Derleme->0,1
+
+//IntelliSense: Kod yazarken açılan yardımcı pencere
+//IntelliCode: Yapay Zeka Kod yazım yardımcısı
