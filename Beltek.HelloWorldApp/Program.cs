2d5340bbd3f8eb4aa14fa43dc861ba3cc0ab3223
GEF-LAB1 <GEF-LAB1@DESKTOP-1V3DLAU>
2024-04-27 12:54:00 +0300

    Try-Catch

--- Beltek.HelloWorldApp/Program.cs
+++ Beltek.HelloWorldApp/Program.cs
@@ -6,72 +6,83 @@ namespace Beltek.HelloWorldApp
         static int number = 10;
         static void Main(string[] args)
         {
-            #region Değişkenler
-            //Console.WriteLine("Adınızı giriniz:");
-            //string isim = Console.ReadLine();
-            //Console.WriteLine("Hoşgeldin," + isim);
 
-            //byte yas;
-            //Console.WriteLine(yas);
-
-            //Int16 sayi = 5;
-            //Int32 _sayi = 5;
-            //Int64 __sayi = 5;
-
-            //byte sayi = 50;//Explicit
-            //var _sayi = 50;//Implicit
-
-            //string isim = "Ahmet";
-            //string soyad = "Mehmet";
-
-            //Console.Write(isim);
-            //Console.WriteLine(soyad);
-
-            //Console.WriteLine("Hoşgeldin, " + isim + " " + soyad);
-            //Console.WriteLine("Hoşgeldin, {0} {1}", isim, soyad);
-            //Console.WriteLine($"Hoşgeldin, {isim} {soyad}"); 
-            #endregion
-
-            Console.WriteLine("Adınızı Giriniz:");
-            string isim = Console.ReadLine();
-            Console.WriteLine("Soyadını Giriniz:");
-            string soyad = Console.ReadLine();
-            Console.WriteLine("Yaşınızı giriniz:");
-            byte yas = byte.Parse(Console.ReadLine());
-            Console.WriteLine($"Adınız:{isim}\nSoyadınız:{soyad}\nYaşınız:{yas}");
-
-
-
-            //****Tür Dönüşümleri(Type Casting)****
-            //Type-Safety
-
-            //byte sayi = 5;
-            //int number = sayi;//Implicit Type Casting
-
-            //checked
-            //{
-            //    int sayi = 257;
-            //    byte number = (byte)sayi;//Explicit Type Casting
-            //    Console.WriteLine(number); 
-            //}
-
-            //string sayi = "25";
-            //byte number = Convert.ToByte(sayi);
-
-            //byte _number = byte.Parse(sayi);
-
-            //int sayi = 50;
-            //object number = sayi;//Boxing
-            //int sayi2 = 10;
-            //int sonuc = (int)number + sayi2;//Unboxing
-
-
-
-            Console.ReadKey();
+            try
+            {
+                Console.WriteLine("Adınızı Giriniz:");
+                string isim = Console.ReadLine();
+                Console.WriteLine("Soyadını Giriniz:");
+                string soyad = Console.ReadLine();
+                Console.WriteLine("Yaşınızı giriniz:");
+                byte yas = byte.Parse(Console.ReadLine());
+                Console.WriteLine($"Adınız:{isim}\nSoyadınız:{soyad}\nYaşınız:{yas}");
+                Console.ReadKey();
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("0-255 arası değer girebilirsiniz.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Yaşınızı sayı ile giriniz");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bilinmeyen bir hata oluştu!");
+            }
         }
     }
 }
+#region Değişkenler
+//Console.WriteLine("Adınızı giriniz:");
+//string isim = Console.ReadLine();
+//Console.WriteLine("Hoşgeldin," + isim);
 
+//byte yas;
+//Console.WriteLine(yas);
+
+//Int16 sayi = 5;
+//Int32 _sayi = 5;
+//Int64 __sayi = 5;
+
+//byte sayi = 50;//Explicit
+//var _sayi = 50;//Implicit
+
+//string isim = "Ahmet";
+//string soyad = "Mehmet";
+
+//Console.Write(isim);
+//Console.WriteLine(soyad);
+
+//Console.WriteLine("Hoşgeldin, " + isim + " " + soyad);
+//Console.WriteLine("Hoşgeldin, {0} {1}", isim, soyad);
+//Console.WriteLine($"Hoşgeldin, {isim} {soyad}"); 
+#endregion
+
+#region Tür Dönüşümleri
+//****Tür Dönüşümleri(Type Casting)****
+//Type-Safety
+
+//byte sayi = 5;
+//int number = sayi;//Implicit Type Casting
+
+//checked
+//{
+//    int sayi = 257;
+//    byte number = (byte)sayi;//Explicit Type Casting
+//    Console.WriteLine(number); 
+//}
+
+//string sayi = "25";
+//byte number = Convert.ToByte(sayi);
+
+//byte _number = byte.Parse(sayi);
+
+//int sayi = 50;
+//object number = sayi;//Boxing
+//int sayi2 = 10;
+//int sonuc = (int)number + sayi2;//Unboxing 
+#endregion
 
 //Solution: İçerisinde birden fazla proje bulundurabilen dosyalardır. Solution dosyaları, bu projelerin disk üzerinde kayıtlı oldugu yer bilgisini tutarlar(Path).
 //namespace: İçinde classları bulunduran yapılardır.
@@ -85,3 +96,7 @@ namespace Beltek.HelloWorldApp
 
 //IntelliSense: Kod yazarken açılan yardımcı pencere
 //IntelliCode: Yapay Zeka Kod yazım yardımcısı
+
+//Debug: Hata ayıklama F5 ile başlatılır.
+
+//Handled Exception: Try catch ile yakalanmış ve exception durumunda yapılacak işlemlerin tanımlandığı durumdur.
